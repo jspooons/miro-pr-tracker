5d9514f5a90c00a59e1bfa2a3d6d5ab352ea128f
Jordan Spooner <jordan.spooner.dev@automation-consultants.com>
2024-01-21 22:37:12 +0000

    change to next script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import RootLayout from "../app/layout";
+import Head from "next/head";
 
 export default function () {
 
   return (
     <div>
+      <Head>
+        <title>Github Pull Request Tracker</title>
+      </Head>
       <div>
         <RootLayout />
       </div>
